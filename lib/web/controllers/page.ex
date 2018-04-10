70de77827954d3971bb71e7fdb933d00f132e407
Jin-Sih Lin <linpct@gmail.com>
2018-04-11 00:16:01 +0800

    use Finix.Controller, 簡化 Controller 寫法

--- lib/web/controllers/page.ex
+++ lib/web/controllers/page.ex
@@ -1,20 +1,25 @@
 defmodule Web.Controllers.Page do
-  def init(opts), do: opts
-
-  def call(conn, opts) do
-    apply __MODULE__, opts[:action], [conn, conn.params]
-  end
-
-  def render(file_name, params) do
-    __ENV__.file
-    |> Path.dirname
-    |> Path.join(["../views/", file_name])
-    |> EEx.eval_file(params)
-  end
-
+#  def init(opts), do: opts
+#
+#  def call(conn, opts) do
+#    apply __MODULE__, opts[:action], [conn, conn.params]
+#  end
+#
+#  def render(file_name, params) do
+#    __ENV__.file
+#    |> Path.dirname
+#    |> Path.join(["../views/", file_name])
+#    |> EEx.eval_file(params)
+#  end
+#
+  use Finix.Controller
+  
   def index(conn, _args) do
-    ret = render "page/index.eex", [world: "hello, world"]
-    conn |> Plug.Conn.put_resp_content_type("text/html") |> Plug.Conn.send_resp(200, ret)
+    ret = render "page/index.eex", [msg: "hello, world"]
+
+    conn
+    |> Plug.Conn.put_resp_content_type("text/html") 
+    |> Plug.Conn.send_resp(200, ret)
   end
 
   def hello(conn, args) do
