3dc5ac1dec87316f41ef62cd443add08f99685f1
Jin-Sih Lin <linpct@gmail.com>
2018-04-11 11:50:26 +0800

    use Finix.Controller 簡化 code

--- lib/web/controllers/page.ex
+++ lib/web/controllers/page.ex
@@ -1,17 +1,4 @@
 defmodule Web.Controllers.Page do
-#  def init(opts), do: opts
-#
-#  def call(conn, opts) do
-#    apply __MODULE__, opts[:action], [conn, conn.params]
-#  end
-#
-#  def render(file_name, params) do
-#    __ENV__.file
-#    |> Path.dirname
-#    |> Path.join(["../views/", file_name])
-#    |> EEx.eval_file(params)
-#  end
-#
   use Finix.Controller
   
   def index(conn, _args) do
