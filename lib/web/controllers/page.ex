5c946718ff8fdfaa490b9edd69e996e9b799c551
Jin-Sih Lin <linpct@gmail.com>
2018-04-12 23:21:55 +0800

    簡化 controller render 語法

--- lib/web/controllers/page.ex
+++ lib/web/controllers/page.ex
@@ -2,11 +2,7 @@ defmodule Web.Controllers.Page do
   use Finix.Controller
   
   def index(conn, _args) do
-    ret = render "page/index.eex", [msg: "hello, world"]
-
-    conn
-    |> Plug.Conn.put_resp_content_type("text/html") 
-    |> Plug.Conn.send_resp(200, ret)
+    render conn, "page/index.eex", [msg: "hello, world"]
   end
 
   def hello(conn, args) do
